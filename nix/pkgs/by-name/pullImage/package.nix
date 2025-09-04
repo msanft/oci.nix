35d327a0c7ce61ea6923672915555289a84ce162
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2025-09-04 10:33:04 +0200

    pullImage: use proper name

--- nix/pkgs/by-name/pullImage/package.nix
+++ nix/pkgs/by-name/pullImage/package.nix
@@ -1,26 +1,40 @@
-{ runCommandNoCC
-, go-containerregistry
+{
+  runCommandNoCC,
+  go-containerregistry,
+  lib,
 }:
 {
   # URL of the image to pull.
   # Of form `<image>:<tag>`.
-  url
-  # Output hash for the FOD. 
-, hash
+  url,
+  # Output hash for the FOD.
+  hash,
   # Whether to allow insecure connections. (i.e. non-TLS)
-, insecure ? false
+  insecure ? false,
   # Platform to pull.
   # Of form `<os>/<arch>`.
   # `null` means all, which is the default.
-, platform ? null
+  platform ? null,
 }:
-runCommandNoCC "pulled-image"
-{
-  buildInputs = [ go-containerregistry ];
-  outputHashAlgo = "sha256";
-  outputHashMode = "recursive";
-  outputHash = hash;
-} ''
-  mkdir -p $out
-  crane pull ${url} $out --format oci ${if insecure then "--insecure" else ""} ${if platform != null then "--platform " + platform else ""}
-''
+let
+  # Brittle parsing, but should work for everyone not using weird tag names
+  name = lib.concatStrings [
+    (builtins.replaceStrings [ ":" ] [ "-" ] (
+      lib.lists.last (lib.strings.splitString "/" (builtins.head (lib.strings.splitString "@" url)))
+    ))
+    "-image"
+  ];
+in
+runCommandNoCC name
+  {
+    buildInputs = [ go-containerregistry ];
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+    outputHash = hash;
+  }
+  ''
+    mkdir -p $out
+    crane pull ${url} $out --format oci ${if insecure then "--insecure" else ""} ${
+      if platform != null then "--platform " + platform else ""
+    }
+  ''
